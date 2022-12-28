169b0ba96ea9dcc925268887f6a3feefa69d1a01
Maciej Oliwa <maciej.oliwa@nokia.com>
2022-12-28 16:14:37 +0100

    WIP

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,26 @@ int main(int argc, char** argv) {
         return x;
     });
 
+    CROW_ROUTE(app, "/hello/<int>")
+    ([](int count){
+        if (count > 100) return crow::response(400);
+        std::ostringstream os;
+        os << count << " bottles of beer!";
+        return crow::response(os.str());
+    });
+
+    CROW_ROUTE(app, "/add_json")
+    .methods("POST"_method)
+    ([](const crow::request& req){
+        auto x = crow::json::load(req.body);
+        if (!x)
+            return crow::response(crow::status::BAD_REQUEST); // same as crow::response(400) <-- this is enumed
+        int sum = x["a"].i() + x["b"].i();
+        std::ostringstream os;
+        os << sum;
+        return crow::response{os.str()};
+    });
+
     app.port(18080).multithreaded().run();
 
     return 0;
